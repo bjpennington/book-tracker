e8a4d05eaed7ad0e0d89be5662c38c23e00f530c
BJ Pennington <bjpennington112@gmail.com>
2018-07-19 14:52:44 -0500

    server side /books post request functional and get sql query updated

--- server/routers/books.router.js
+++ server/routers/books.router.js
@@ -4,7 +4,9 @@ const pool = require('../modules/pool');
 
 // BOOKS GET REQUEST
 router.get('/', (req, res) => {
-    let queryText = `SELECT * FROM "books";`;
+    let queryText = `SELECT "books"."id", "books"."title", "books"."author", "books"."image_url", "books"."publication_date", "books"."number_of_pages", "books"."favorite", "genres"."genre_name"
+                    FROM "books"
+                    JOIN "genres" ON "books"."genre_id" = "genres"."id";`;
     pool.query(queryText)
         .then(results => {  
             res.send(results.rows);
@@ -16,6 +18,20 @@ router.get('/', (req, res) => {
 });
 
 // BOOKS POST REQUEST
+router.post('/', (req, res) => {
+    let bookToAdd = req.body;   
+    let queryText = `INSERT INTO "books"
+                    ("title", "author", "image_url", "publication_date", "number_of_pages", "genre_id")
+                    VALUES ($1, $2, $3, $4, $5, $6);`;
+    pool.query(queryText, [bookToAdd.title, bookToAdd.author, bookToAdd.image_url, bookToAdd.publication_date, bookToAdd.number_of_pages, bookToAdd.genre_id])
+        .then(results => {
+            res.sendStatus(201);
+        })
+        .catch(errorFromPG => {
+            console.log('/books POST error', errorFromPG);
+            res.sendStatus(500);
+        });
+});
 
 // BOOKS DELETE REQUEST
 
