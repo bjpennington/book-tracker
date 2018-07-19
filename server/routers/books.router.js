144fed3391f0ca5e7e7c1a1376ed5f78f044c8c6
BJ Pennington <bjpennington112@gmail.com>
2018-07-19 14:23:43 -0500

    server side /books get request functional

--- server/routers/books.router.js
+++ server/routers/books.router.js
@@ -3,6 +3,17 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 // BOOKS GET REQUEST
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "books";`;
+    pool.query(queryText)
+        .then(results => {  
+            res.send(results.rows);
+        })
+        .catch(errorFromPG => {
+            console.log('/books GET error:', errorFromPG);  
+            res.sendStatus(500); 
+        });
+});
 
 // BOOKS POST REQUEST
 
